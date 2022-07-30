921e40af568ce87209e87d1fd443685ab9280d34
Chris Jackson <89106051+Chris353536345373344@users.noreply.github.com>
2022-07-30 15:54:07 -0400

    Add files via upload

---
+++ Chesseburgerdatabase.py
@@ -0,0 +1,40 @@
+from asyncio import exceptions
+from distutils.command.build_ext import extension_name_re
+import os
+import random
+import time
+#hamburber :yum:
+
+#some code to mess with others
+for x in range(20):
+    print("Loading BSlib\nLoading LMaOlib\nLoading lollib")
+    time.sleep(.01)
+print("Injecting autoloader\nInjecting WannaCry\nEncrypting Files\nComplete")
+time.sleep(.4)
+os.system("cls")
+
+#actual code
+prefix = ("IMG")
+randn = random.randint(0,26)
+extension = (".jpg")
+photo = f"{prefix}{randn}{extension}"
+
+
+
+os.chdir("photos")
+os.startfile(photo)
+
+
+#flavor text for "secrets"
+if (randn == 0):
+    print("Wow haha funny easter egg (thanks for the image slebbey)")
+    input("little pause so you can read (press enter)")
+
+if(randn == 20):
+    print("A cheesburger horde apears!")
+    input("press ener")
+
+if (randn == 25):
+    print("Bunger! Bunger!")
+    input("press enter")
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+Generates some wacky hamburger images (only 26 at this time)
